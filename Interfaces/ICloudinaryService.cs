28d78aa740ad10d3eb6a89b507e62df1d9cbc672
Irfan Yousuf <irfanusuf33@gmail.com>
2025-02-27 21:10:33 +0530

    update

--- Interfaces/ICloudinaryService.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace WebApplication1.Interfaces;
-
-public interface ICloudinaryService
-{
-Task<string> UploadImageAsync(IFormFile file);
-
-}
--- Interfaces/IMailService.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace WebApplication1.Interfaces;
-
-public interface IMailService
-{
-Task SendEmailAsync(string to, string subject, string body, bool isHtml = true);
-}
